60237a94f737b50bf3ab935d4e98ad42d27518f3
elliot <elliotcs30@gmail.com>
2023-04-13 08:29:45 +0800

    feat: add #9.1.1 osgetcwd

---
+++ programing-bible/9.1.1 osgetcwd.py
@@ -0,0 +1,6 @@
+# os 模組
+# os 提供取得工作目錄、建立目錄、刪除目錄、刪除檔案、執行作業系統命令等函式，使用時必須匯入 os 模組。
+
+# getcwd 函式可以取得目前的工作目錄。
+import os
+print(os.getcwd()) # C:\...\...\Doc...\Git...\python-training\programing-bible
