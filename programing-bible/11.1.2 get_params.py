71e8507b32815acd71224719d1a1938dad4286f9
elliot <elliotcs30@gmail.com>
2023-04-17 17:30:17 +0800

    feat: add #11.1.2 get params request text

--- programing-bible/11.1.2 get_params.py
+++ programing-bible/11.1.2 get_params.py
@@ -6,3 +6,22 @@ payload = {'key1': 'value1', 'key2': 'value2'}
 
 r = requests.get('http://httpbin.org/get', params=payload)
 print(r) # <Response [200]>
+print(r.text)
+
+'''
+{
+  "args": {
+    "key1": "value1",
+    "key2": "value2"
+  },
+  "headers": {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Host": "httpbin.org",
+    "User-Agent": "python-requests/2.23.0",
+    "X-Amzn-Trace-Id": "Root=1-643d1112-57a6e45f2ff3d2d00fd76e69"
+  },
+  "origin": "210.63.211.145",
+  "url": "http://httpbin.org/get?key1=value1&key2=value2"
+}
+'''
