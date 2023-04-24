33bd73b09b91dd6c499e0bc7441f40ea1ab68096
Elliot <elliotcs30@gmail.com>
2023-04-24 23:24:31 +0800

    feat: add #12.3.5 json dump

---
+++ programing-bible/12.3.5 jsondump1.py
@@ -0,0 +1,9 @@
+# dumps 輸出 json 字串
+
+import json
+
+with open('class_str.json', 'r', encoding='utf-8') as f:
+    datas = json.load(f)
+print(datas, type(datas))
+dumpdata = json.dumps(datas, ensure_ascii=False)
+print(dumpdata, type(dumpdata))
