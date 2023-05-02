e880022c78ed8688c1471faad0077c5d779f13bf
Elliot <elliotcs30@gmail.com>
2023-05-02 19:56:57 +0800

    feat: add #12.5.7 sqlite fetchall method

---
+++ programing-bible/12.5.7 fetchall.py
@@ -0,0 +1,15 @@
+import sqlite3
+conn = sqlite3.connect('test.sqlite') # 建立資料庫連線
+
+cursor = conn.execute('select * from contact')
+rows = cursor.fetchall()
+
+# 顯示原始資料
+print(rows)
+
+# 逐筆資料顯示
+for row in rows:
+    print(row[0], row[1])
+
+conn.commit()
+conn.close()
