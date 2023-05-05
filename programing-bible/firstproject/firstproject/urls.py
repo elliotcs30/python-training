7ac478c3e57af8845687db674221d36020bb13b2
Elliot <elliotcs30@gmail.com>
2023-05-05 23:23:26 +0800

    feat: add new router

--- programing-bible/firstproject/firstproject/urls.py
+++ programing-bible/firstproject/firstproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import sayhello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', sayhello),
 ]
