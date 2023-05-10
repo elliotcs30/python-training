b0703f5d251fd2c0bcdbe27e306963c60c410151
Elliot <elliotcs30@gmail.com>
2023-05-10 23:16:29 +0800

    feat: add model admin class

--- programing-bible/students/studentsapp/admin.py
+++ programing-bible/students/studentsapp/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from studentsapp.models import student
 
 # Register your models here.
-admin.site.register(student)
+# 第一種方式，加入 ModelAdmin 類別，定義顯示欄位
+# admin.site.register(student)
 
+# 第二種方式，加入 ModelAdmin 類別，定義顯示欄位
+# class studentAdmin(admin.ModelAdmin):
+#     list_display=('id', 'cName', 'cSex', 'cBirthday', 'cEmail', 'cPhone', 'cAddr')
+# admin.site.register(student, studentAdmin)
+
+# 第三種方式，加入 ModelAdmin 類別，定義顯示欄位、欄位過濾資料、搜尋和排序
+class studentAdmin(admin.ModelAdmin):
+    list_display=('id', 'cName', 'cSex', 'cBirthday', 'cEmail', 'cPhone', 'cAddr')
+    list_filter=('cName', 'cSex')
+    search_fields=('cName',)
+    ordering=('id',)
+
+admin.site.register(student, studentAdmin)
