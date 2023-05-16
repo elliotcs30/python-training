e75734bceb2c4898dd34f09bf8792b8033aaf2b3
Elliot <elliotcs30@gmail.com>
2023-05-17 00:04:06 +0800

    fix: can't show dice page

--- programing-bible/web/web/urls.py
+++ programing-bible/web/web/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import dice
+from webapp.views import sayhello, dice
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dice/', dice)
+    path('dice/', dice),
 ]
